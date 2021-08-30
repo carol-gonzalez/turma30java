147460920f2af8fcf8f6fdfb34b2d02d4e1d1a5a
carol-gonzalez <98carolinagonzalez@gmail.com>
2021-08-30 20:52:01 -0300

    Atividade CRUD Back-end Minha Loja de Games

---
+++ spring/minhaLojaDeGames/minhaLojaDeGames/src/main/java/com/minhalojadegames/minhaLojaDeGames/repository/CategoriaRepository.java
@@ -0,0 +1,10 @@
+package com.minhalojadegames.minhaLojaDeGames.repository;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.minhalojadegames.minhaLojaDeGames.model.Categoria;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long>{
+	public List<Categoria> findAllByDescricaoContainingIgnoreCase(String descricao);
+}
