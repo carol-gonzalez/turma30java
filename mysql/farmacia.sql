f7f53d1affcd279bd21b37d21d47d25585b1721c
carol-gonzalez <98carolinagonzalez@gmail.com>
2021-09-03 20:13:32 -0300

    Banco de Dados Exercício da Farmácia

---
+++ mysql/farmacia.sql
@@ -0,0 +1,88 @@
+-- MySQL dump 10.13  Distrib 8.0.25, for Win64 (x86_64)
+--
+-- Host: localhost    Database: farmacia
+-- ------------------------------------------------------
+-- Server version	8.0.25
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `tb_categoria`
+--
+
+DROP TABLE IF EXISTS `tb_categoria`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tb_categoria` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `descricao` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_categoria`
+--
+
+LOCK TABLES `tb_categoria` WRITE;
+/*!40000 ALTER TABLE `tb_categoria` DISABLE KEYS */;
+INSERT INTO `tb_categoria` VALUES (1,'Oftamológicos');
+/*!40000 ALTER TABLE `tb_categoria` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tb_produto`
+--
+
+DROP TABLE IF EXISTS `tb_produto`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tb_produto` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `estoque` int NOT NULL,
+  `preco` decimal(19,2) NOT NULL,
+  `remedio` varchar(80) DEFAULT NULL,
+  `categoria_id` bigint DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK1ksfe2oumgjxke3oc5op41lej` (`categoria_id`),
+  CONSTRAINT `FK1ksfe2oumgjxke3oc5op41lej` FOREIGN KEY (`categoria_id`) REFERENCES `tb_categoria` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_produto`
+--
+
+LOCK TABLES `tb_produto` WRITE;
+/*!40000 ALTER TABLE `tb_produto` DISABLE KEYS */;
+INSERT INTO `tb_produto` VALUES (1,200,8.99,'Rivotril',NULL),(2,300,42.99,'Carbolitium',NULL),(3,150,38.99,'Sertralina',NULL),(4,100,30.49,'Respiridona',NULL),(5,90,25.99,'Zolpidem',NULL),(7,80,12.99,'Regencel',NULL);
+/*!40000 ALTER TABLE `tb_produto` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping events for database 'farmacia'
+--
+
+--
+-- Dumping routines for database 'farmacia'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-09-03 20:12:15
